513889e10618b21f5570dee5e941d3597d9ac8ab
Greg Molnar <molnargerg@gmail.com>
2015-10-17 14:45:07 +0200

    convert test to use minitest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,8 @@
 require 'rubygems'
-require 'test/unit'
 require 'bundler/setup'
-require 'test_declarative'
+# require 'test_declarative'
+require 'minitest/autorun'
+require 'minitest/spec'
 
 $: << File.expand_path('../../lib', __FILE__)
 require 'i18n/missing_translations'
